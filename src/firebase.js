08f4838b65f2a1c309ae757a9924956329fdca9b
Onurr23 <halitonurkara18@gmail.com>
2021-01-02 15:31:40 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCq_cWvNXjmluUKRy_gmF007InaO7MfYyo",
+    authDomain: "challenge-fcf46.firebaseapp.com",
+    projectId: "challenge-fcf46",
+    storageBucket: "challenge-fcf46.appspot.com",
+    messagingSenderId: "985550680641",
+    appId: "1:985550680641:web:724bf4e439e3c280c84cc0",
+    measurementId: "G-782HQJPYRT"
+  };
